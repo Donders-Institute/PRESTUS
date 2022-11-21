d2c85ba540b74ea1b2c2b810f521d6195f36d7c0
Andrey Chetverikov <achetverikov@users.noreply.github.com>
2022-11-21 12:43:57 +0100

    updated plot_coronal_slices

--- project/tuSIM/functions/plot_coronal_slices.m
+++ project/tuSIM/functions/plot_coronal_slices.m
@@ -1,6 +1,9 @@
-function plot_coronal_slices(img)
-im_center = round(size(img)/2);
-%img = (img-min(img(:)))/(max(img(:))-min(img(:)))*255;
+function plot_coronal_slices(img, coord)
+arguments
+    img (:,:,:)
+    coord (3,1) = round(size(img)/2)
+end
+img_rescaled = round(rescale(img, 1, 255));
 figure
-montage({squeeze(img(im_center(1),:,:)),squeeze(img(:,im_center(2),:)),squeeze(img(:,:,im_center(3)))},gray, 'Size',[1 3])
+montage({squeeze(img_rescaled(coord(1),:,:))',squeeze(img_rescaled(:,coord(2),:))',squeeze(img_rescaled(:,:,coord(3)))'},gray, 'Size',[1 3])
 end
