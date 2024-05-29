cf4334dfe609ef4137ff429e4ba118d67333ec1e
Julian Kosciessa <julian.kosciessa@donders.ru.nl>
2024-05-29 15:20:21 +0200

    make heating video creation and saving optional (default: yes)
    parameters.heatingvideo

--- functions/plot_heating_sims.m
+++ functions/plot_heating_sims.m
@@ -21,6 +21,8 @@ function plot_heating_sims(focal_planeT, time_status_seq, parameters, trans_pos,
     end
     focal_axis_temperature = squeeze(focal_planeT(trans_pos(1),:,:));
     
+    %% plot temperature rise
+
     h = figure
     plot([time_status_seq(find([time_status_seq.recorded]==1)).time], focal_axis_temperature(1:10:size(focal_axis_temperature, 1), 1:size(time_status_seq,2))'); % plot every 10th voxel   
     colormap('lines')
@@ -42,7 +44,8 @@ function plot_heating_sims(focal_planeT, time_status_seq, parameters, trans_pos,
     saveas(h, output_plot, 'png')
     close(h)
     
-  
+    %% plot CEM43
+
     g = figure
     focal_CEM = squeeze(CEM43(trans_pos(1),:,1:numel(time_status_seq)));
 
@@ -66,40 +69,41 @@ function plot_heating_sims(focal_planeT, time_status_seq, parameters, trans_pos,
     saveas(g, output_plotCEM, 'png')
     close(g)
 
-    %% Creates a video of heating effects over the course of the experiment
+    %% Create a video of heating effects over the course of the experiment (request by default)
 
-    color_limits = [min(focal_planeT(:)), max(focal_planeT(:))];
-    brain_slice = mat2gray(squeeze(brain_img(:,parameters.transducer.pos_grid(2),:)));
-
-    output_video_name = fullfile(parameters.output_dir,sprintf('sub-%03d_%s_heating_animation%s.avi', parameters.subject_id, parameters.simulation_medium, parameters.results_filename_affix));
-    v = VideoWriter(output_video_name,'Uncompressed AVI');
-    v.FrameRate = 2; % frames per second
-
-    % Create a video writer object for the output video file and open the object for writing.
-    open(v);
-    tss_recorded = time_status_seq(find([time_status_seq.recorded]==1));
-    %Generate a set of frames, get the frame from the figure, and then write each frame to the file.
-    gray_img_brain = repmat(mat2gray(brain_slice),[1 1 3]);
-    for k = 1:size(focal_planeT,3) 
-        cur_temp = squeeze(focal_planeT(:, :,  k));
-        rgbImage = ind2rgb(round((cur_temp-color_limits(1))/diff(color_limits)*256), viridis(256));
-        rgbImage(cur_temp <=37.01) = nan;
-        imshowpair(gray_img_brain, rgbImage, 'blend')
-        %set(h, 'AlphaData', 0.2)
-        try
-            title(sprintf('Timepoint: %i, time: %.2f s, max temperature: %.2f C, transducer: %s', ...
-                k, tss_recorded(k).time, max(cur_temp(:)), tss_recorded(k).status), 'FontSize', 10)
-        catch
-            warning("Some parameter is out of bounds")
-            disp(['k:', num2str(k)]);
-            disp(['dim focal plane (3):', num2str(size(focal_planeT,3))]);
-            disp(['dim tss_recorded:', num2str(size(tss_recorded) )]);
-        end
-        frame = getframe(gcf);
-        writeVideo(v,frame);
+    if ~isfield(parameters, 'heatingvideo')
+        parameters.heatingvideo = 1; % default: create & save video
     end
-
-    close(v);
-    close;
+    if parameters.heatingvideo == 1
+        color_limits = [min(focal_planeT(:)), max(focal_planeT(:))];
+        brain_slice = mat2gray(squeeze(brain_img(:,parameters.transducer.pos_grid(2),:)));
+        output_video_name = fullfile(parameters.output_dir,sprintf('sub-%03d_%s_heating_animation%s.avi', parameters.subject_id, parameters.simulation_medium, parameters.results_filename_affix));
+        v = VideoWriter(output_video_name,'Uncompressed AVI');
+        v.FrameRate = 2; % frames per second
+        % Create a video writer object for the output video file and open the object for writing.
+        open(v);
+        tss_recorded = time_status_seq(find([time_status_seq.recorded]==1));
+        %Generate a set of frames, get the frame from the figure, and then write each frame to the file.
+        gray_img_brain = repmat(mat2gray(brain_slice),[1 1 3]);
+        for k = 1:size(focal_planeT,3) 
+            cur_temp = squeeze(focal_planeT(:, :,  k));
+            rgbImage = ind2rgb(round((cur_temp-color_limits(1))/diff(color_limits)*256), viridis(256));
+            rgbImage(cur_temp <=37.01) = nan;
+            imshowpair(gray_img_brain, rgbImage, 'blend')
+            %set(h, 'AlphaData', 0.2)
+            try
+                title(sprintf('Timepoint: %i, time: %.2f s, max temperature: %.2f C, transducer: %s', ...
+                    k, tss_recorded(k).time, max(cur_temp(:)), tss_recorded(k).status), 'FontSize', 10)
+            catch
+                warning("Some parameter is out of bounds")
+                disp(['k:', num2str(k)]);
+                disp(['dim focal plane (3):', num2str(size(focal_planeT,3))]);
+                disp(['dim tss_recorded:', num2str(size(tss_recorded) )]);
+            end
+            frame = getframe(gcf);
+            writeVideo(v,frame);
+        end
+        close(v);
+    end
     
 end
