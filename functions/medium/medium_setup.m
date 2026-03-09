3ccec9fd3597589a844f6f73713d69bfdbfa1d9d
Julian Kosciessa <julian.kosciessa@donders.ru.nl>
2026-03-09 17:54:12 +0100

    [clean] medium mapping

--- functions/medium/medium_setup.m
+++ functions/medium/medium_setup.m
@@ -26,8 +26,8 @@ function kwave_medium = medium_setup(parameters, medium_masks, planimg, pseudoCT
     density = empty_grid;
     alpha_coeff = empty_grid;
     alpha_power = empty_grid;
-    thermal_conductivity = empty_grid;       % [W/(m.K)]
-    specific_heat = empty_grid;            % [J/(kg.K)]
+    thermal_conductivity = empty_grid;
+    specific_heat = empty_grid;
     perfusion = empty_grid;
     absorption_fraction = ones(parameters.grid_dims); % default: convert 100% of attenuation into absorption
     temp_0 = empty_grid;
@@ -116,8 +116,8 @@ function kwave_medium = medium_setup(parameters, medium_masks, planimg, pseudoCT
             clear skull_idx
 
         else
-            thermal_conductivity(medium_masks==medium_i) = medium.(label_name).thermal_conductivity;                 % [W/(m.K)]
-            specific_heat(medium_masks==medium_i) = medium.(label_name).specific_heat_capacity;                      % [J/(kg.K)]
+            thermal_conductivity(medium_masks==medium_i) = medium.(label_name).thermal_conductivity;
+            specific_heat(medium_masks==medium_i) = medium.(label_name).specific_heat_capacity;
             sound_speed(medium_masks==medium_i) = medium.(label_name).sound_speed; 
             density(medium_masks==medium_i) = medium.(label_name).density;
             alpha_coeff(medium_masks==medium_i) = medium.(label_name).alpha_coeff; 
@@ -129,12 +129,15 @@ function kwave_medium = medium_setup(parameters, medium_masks, planimg, pseudoCT
                 absorption_fraction(medium_masks==medium_i) = medium.(label_name).absorption_fraction;
             end
             if isfield(parameters.thermal.temp_0, label_name)
-                temp_0(medium_masks==medium_i) = parameters.thermal.temp_0.(label_name);                             % [degC]
+                temp_0(medium_masks==medium_i) = parameters.thermal.temp_0.(label_name);
             end
         end
     end
 
-    % account for k-Wave's actual attenuation behaviour
+    % convert perfusion rate [mL/min/kg] into perfusion coefficient [1/s]
+    perfusion_coeff = (perfusion ./ 60) .* density * 1e-6;
+
+    %% account for k-Wave's actual attenuation behaviour
     % limits discrepancies for high attenuation estimates (see https://doi.org/10.1121/1.4894790).
     % 'alpha_coeff' is rescaled to match the specified alpha_coeff and alpa_power_true for the center frequency
 
@@ -177,10 +180,7 @@ function kwave_medium = medium_setup(parameters, medium_masks, planimg, pseudoCT
         alpha_power_fixed = alpha_power;
     end
 
-    % convert perfusion rate [mL/min/kg] into perfusion coefficient [1/s]
-    perfusion_coeff = (perfusion ./ 60) .* density * 1e-6; % [1/s]
-
-    % specify the medium as a kWave-compatible structure 
+    %% specify the medium as a kWave-compatible structure 
     % Note: absorption_fraction and temp_0 need to be removed later)
     kwave_medium = struct('sound_speed', sound_speed, ...
                           'density', density, ...
@@ -192,50 +192,45 @@ function kwave_medium = medium_setup(parameters, medium_masks, planimg, pseudoCT
                           'absorption_fraction', absorption_fraction,...
                           'temp_0', temp_0);
     
-    % save images for debugging
+    %% [debug] save acoustic property images
     if parameters.debug == 1
+        % save raw medium matrices as niftis
         try
             filename_density = fullfile(parameters.debug_dir, sprintf('matrix_density'));
-            niftiwrite(density, filename_density, 'Compressed',true);
-            pause(0.1);
+            niftiwrite(density, filename_density, 'Compressed',true); pause(0.1);
             filename_sound_speed = fullfile(parameters.debug_dir, sprintf('matrix_sound_speed'));
-            niftiwrite(sound_speed, filename_sound_speed, 'Compressed',true);
-            pause(0.1);
+            niftiwrite(sound_speed, filename_sound_speed, 'Compressed',true); pause(0.1);
             filename_alpha_coeff = fullfile(parameters.debug_dir, sprintf('matrix_alpha_coeff'));
-            niftiwrite(alpha_coeff, filename_alpha_coeff, 'Compressed',true);
-            pause(0.1);
+            niftiwrite(alpha_coeff, filename_alpha_coeff, 'Compressed',true); pause(0.1);
             filename_alpha_power = fullfile(parameters.debug_dir, sprintf('matrix_alpha_power'));
-            niftiwrite(alpha_power, filename_alpha_power, 'Compressed',true);
-            pause(0.1);
+            niftiwrite(alpha_power, filename_alpha_power, 'Compressed',true); pause(0.1);
             filename_alpha_coeff_fixed = fullfile(parameters.debug_dir, sprintf('matrix_alpha_coeff_fixed'));
-            niftiwrite(alpha_coeff_fixed, filename_alpha_coeff_fixed, 'Compressed',true);
-            pause(0.1);
+            niftiwrite(alpha_coeff_fixed, filename_alpha_coeff_fixed, 'Compressed',true); pause(0.1);
             filename_perfusion = fullfile(parameters.debug_dir, sprintf('matrix_perfusion'));
-            niftiwrite(perfusion_coeff, filename_perfusion, 'Compressed',true);
-            pause(0.1);
+            niftiwrite(perfusion_coeff, filename_perfusion, 'Compressed',true); pause(0.1);
             filename_absorption = fullfile(parameters.debug_dir, sprintf('matrix_absorption'));
-            niftiwrite(absorption_fraction, filename_absorption, 'Compressed',true);
+            niftiwrite(absorption_fraction, filename_absorption, 'Compressed',true); pause(0.1);
         catch
             warning("Error with saving debug images: medium mapping. May result from concurrent write attempts...")
         end
-    end
 
-    % save images of assigned medium properties
-    if contains(parameters.simulation_medium, {'layered'}) && parameters.debug == 1 && (exist('planimg') & ~isempty(planimg))
-        medium_properties_nifti(parameters, kwave_medium, planimg.inv_transf, planimg.t1_header, 'sound_speed')
-        medium_properties_nifti(parameters, kwave_medium, planimg.inv_transf, planimg.t1_header, 'density')
-        medium_properties_nifti(parameters, kwave_medium, planimg.inv_transf, planimg.t1_header, 'alpha_coeff')
-        medium_properties_nifti(parameters, kwave_medium, planimg.inv_transf, planimg.t1_header, 'alpha_power')
-        medium_properties_nifti(parameters, kwave_medium, planimg.inv_transf, planimg.t1_header, 'thermal_conductivity')
-        medium_properties_nifti(parameters, kwave_medium, planimg.inv_transf, planimg.t1_header, 'specific_heat')
-        medium_properties_nifti(parameters, kwave_medium, planimg.inv_transf, planimg.t1_header, 'perfusion_coeff')
-        medium_properties_nifti(parameters, kwave_medium, planimg.inv_transf, planimg.t1_header, 'absorption_fraction')
-    end
+        % save images of assigned medium properties with proper headers
+        if contains(parameters.simulation_medium, {'layered'}) && (exist('planimg') & ~isempty(planimg))
+            medium_properties_nifti(parameters, kwave_medium, planimg.inv_transf, planimg.t1_header, 'sound_speed')
+            medium_properties_nifti(parameters, kwave_medium, planimg.inv_transf, planimg.t1_header, 'density')
+            medium_properties_nifti(parameters, kwave_medium, planimg.inv_transf, planimg.t1_header, 'alpha_coeff')
+            medium_properties_nifti(parameters, kwave_medium, planimg.inv_transf, planimg.t1_header, 'alpha_power')
+            medium_properties_nifti(parameters, kwave_medium, planimg.inv_transf, planimg.t1_header, 'thermal_conductivity')
+            medium_properties_nifti(parameters, kwave_medium, planimg.inv_transf, planimg.t1_header, 'specific_heat')
+            medium_properties_nifti(parameters, kwave_medium, planimg.inv_transf, planimg.t1_header, 'perfusion_coeff')
+            medium_properties_nifti(parameters, kwave_medium, planimg.inv_transf, planimg.t1_header, 'absorption_fraction')
+        end
 
-    % save a pCT if used
-    if parameters.use_pseudoCT == 1 && parameters.debug == 1
-        filename_pct = fullfile(parameters.debug_dir, sprintf('pct%s', parameters.results_filename_affix));
-        niftiwrite(pseudoCT, filename_pct, 'Compressed',true);
+        % save a pCT (if used)
+        if parameters.use_pseudoCT == 1
+            filename_pct = fullfile(parameters.debug_dir, sprintf('pct%s', parameters.results_filename_affix));
+            niftiwrite(pseudoCT, filename_pct, 'Compressed',true);
+        end
     end
 
 end
