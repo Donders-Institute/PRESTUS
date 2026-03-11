8d6546ed83545eb8dee50bd77797afe153131672
Julian Kosciessa <julian.kosciessa@donders.ru.nl>
2026-03-11 21:50:17 +0100

    [beauty] simplify variable passing

--- functions/medium/medium_pct_soundspeed.m
+++ functions/medium/medium_pct_soundspeed.m
@@ -1,4 +1,4 @@
-function [sound_speed] = medium_pct_soundspeed(parameters, medium, sound_speed, density, pseudoCT, skull_idx, algorithm)
+function [sound_speed] = medium_pct_soundspeed(parameters, sound_speed, density, pseudoCT, skull_idx, algorithm)
 
 switch algorithm
     case 'k-plan'
@@ -7,9 +7,9 @@ switch algorithm
 
     case 'marsac'
 
-        c_water       = medium.water.sound_speed;     % sound speed [m/s]
+        c_water       = parameters.medium.water.sound_speed;     % sound speed [m/s]
         c_skull       = 3360;     % max. speed of sound in skull [m/s]
-        rho_water     = medium.water.density;      % density [kg/m^3]
+        rho_water     = parameters.medium.water.density;      % density [kg/m^3]
         rho_bone      = 2100;     % max. skull density [kg/m3]
 
         sound_speed(skull_idx) = c_water + (c_skull - c_water) * ...
@@ -17,8 +17,8 @@ switch algorithm
         
     case 'aubry'
 
-        c_water = medium.water.sound_speed;
-        c_bone = medium.skull.sound_speed;
+        c_water = parameters.medium.water.sound_speed;
+        c_bone = parameters.medium.skull.sound_speed;
 
         phi(skull_idx) = 1-(pseudoCT(skull_idx)/max(pseudoCT(skull_idx)));
         sound_speed(skull_idx) = c_water * phi(skull_idx) + ...
@@ -28,7 +28,7 @@ switch algorithm
 
     case 'none'
 
-        sound_speed(skull_idx) = medium.skull.sound_speed;
+        sound_speed(skull_idx) = parameters.medium.skull.sound_speed;
 
     otherwise
         error("Specified CT sound speed mapping is not supported.")
