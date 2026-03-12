06847b2de3f1c9f49cf97749e75bc21f301e61e6
Julian Kosciessa <julian.kosciessa@donders.ru.nl>
2026-03-12 17:42:48 +0100

    [tp] reorder steps, idenitfy missing plot inputs

--- functions/transducer/tp_plot_heuristic_transducer.m
+++ functions/transducer/tp_plot_heuristic_transducer.m
@@ -1,20 +1,50 @@
-function tp_plot_heuristic_transducer(tpos_pars, img_orig, img_info, parameters, pixel_size, target_name, subject_id)
+function tp_plot_heuristic_transducer(tpos_pars, img, img_info, parameters, pixel_size, target_name, subject_id)
 % TP_PLOT_HEURISTIC_TRANSDUCER Create visualization of heuristic transducer placement
 %
 % Selects heuristic transducer position based on intersection, skull/skin distance criteria. Creates comprehensive
 % 6-panel figure showing aligned anatomy, geometric properties, 3D rendering, and validation plots.
 %
 % INPUT
-%   tpos_pars    - Table from tp_evaluate_candidate_positions_gpu()
-%   img_orig     - Original segmented head image
+%   tpos_pars    - Table from tp_evaluate_candidate_positions
+%   img          - Segmented head image
 %   img_info     - NIfTI header info  
 %   parameters   - Parameters struct
 %   pixel_size   - Scalar voxel size in mm
 %   target_name  - String target identifier
 %   subject_id   - Scalar subject ID
 %
+% Missing:
+% tp_struct.outer_sphere_3d   % Skull intersection sphere (from `tp_find_candidate_positions`)
+% tp_struct.segm_img_slice    % 2D visualization slice with overlays
+% tp_struct.target            % Original target position [x y z] (vs `target_xyz` from table)
+% tp_struct.coord_mesh.xyz    % Flattened voxel coordinates
+% tp_struct.norm_v            % Unit normal vectors for all candidates
+% tp_struct.d                 % Plane equation constant
+% tp_struct.max_od_grid       % Aperture diameter in voxels
+% tp_struct.ex_plane_pos_all  % Exit plane positions array
+% tp_struct.shifted_trans_pos_coords % Candidate transducer positions
+% tp_struct.skin_boundary     % Skin surface mask
+%
 % OUTPUT
 %   Saves: sub-XXX_optimal_TARGET.png
+
+% collect inputs from passing structure
+
+outer_sphere_3d = tp_struct.outer_sphere_3d;
+segm_img_slice = tp_struct.segm_img_slice;
+target = tp_struct.target;
+coord_mesh.xyz = tp_struct.coord_mesh.xyz;
+norm_v = tp_struct.norm_v;
+d = tp_struct.d;
+max_od_grid = tp_struct.max_od_grid;
+ex_plane_pos_all = tp_struct.ex_plane_pos_all;
+shifted_trans_pos_coords = tp_struct.shifted_trans_pos_coords;
+skin_boundary = tp_struct.skin_boundary;
+
+geom_focus_pos_all = shifted_trans_pos_coords - norm_v*(parameters.transducer.curv_radius_mm/pixel_size);
+
+sz = size(img);  
+[t1_x, t1_y, t1_z] = ndgrid(1:sz(1), 1:sz(2), 1:sz(3));  % Full volume voxel coordinates
 
 % Select optimal position (original multi-step criteria)
 tppf = tpos_pars(tpos_pars.prop_intersect<0.05,:);
@@ -22,12 +52,16 @@ tppf = tppf(tppf.mean_dist_skull <= quantile(tppf.mean_dist_skull, 0.5) & ...
            tppf.var_dist_skull <= quantile(tppf.var_dist_skull, 0.1),:);
 tppf = tppf(tppf.var_dist_skin==min(tppf.var_dist_skin),:);
 i = 1; % First optimal candidate
+
 target_xyz = [tppf.targ_x(i), tppf.targ_y(i), tppf.targ_z(i)];
 trans_xyz = [tppf.trans_x(i), tppf.trans_y(i), tppf.trans_z(i)];
 
+ex_pl_xyz = gather(ex_plane_pos_all(i,:));
+gf_xyz = gather(geom_focus_pos_all(i,:));
+
 % Align to focal axis (original preprocessing)
 [rotated_img, trans_xyz, target_xyz, transformation_matrix, ~, ~, ~, ~] = ...
-    preproc_align_to_focal_axis(img_orig, img_info, round(trans_xyz)', target_xyz', 1, parameters);
+    preproc_align_to_focal_axis(img, img_info, round(trans_xyz)', target_xyz', 1, parameters);
 TF = maketform('affine', transformation_matrix);
 
 % 6-panel comprehensive visualization
@@ -86,7 +120,6 @@ h = figure('units', 'normalized', 'position', [0 0 1 1]);
         skin_idx = find(skin_boundary);
         max_od_grid = max(parameters.transducer.Elements_OD_mm)/pixel_size;
 
-        %show_3d_head(segmented_img_orig, target, shifted_trans_pos_coords(i,:), parameters, pixel_size, coord_mesh.xyz, [-1 1 0],[0,0],0)
         show_3d_head(segmented_img_orig, target, shifted_trans_pos_coords(i,:), parameters, pixel_size, coord_mesh.xyz, [0 0 0],[0,0],0)
 
         [t1_x_r, t1_y_r, t1_z_r] = ndgrid(1:size(rotated_img, 1),1:size(rotated_img, 2),1:size(rotated_img, 3));
@@ -98,7 +131,6 @@ h = figure('units', 'normalized', 'position', [0 0 1 1]);
     subplot(2,3,4);
 
         norm_v_r = (trans_xyz-target_xyz)/norm(trans_xyz-target_xyz);
-        %norm_v_r = norm_v_r';
         d = sum(norm_v_r.*ex_pl_xyz);
         orth_plane_disk = find(abs(sum(t1_xyz_r.*norm_v_r,2)-d)<0.5);
 
@@ -108,18 +140,6 @@ h = figure('units', 'normalized', 'position', [0 0 1 1]);
         orth_plane_3d = smooth3(orth_plane_3d);
 
         segmented_img_to_plot = rotated_img;
-        % sM = size(segmented_img_to_plot);
-        % N = norm_v(i,:);
-        % N_orth = null(N(:).')
-        % N_orth = N_orth(:,1)
-        % segmented_img_to_plot(:,floor(target_xyz(2)):size(segmented_img_to_plot,2),:) = [];
-        %segmented_img_to_plot(:,:,floor(target_xyz(3)):size(segmented_img_to_plot,3)) = [];
-
-        % Dist = reshape(((1:sM(1)) - target(1)) * N_orth(1), [sM(1), 1, 1]) + ...
-        %        reshape(((1:sM(2)) - target(2)) * N_orth(2), [1, sM(2), 1]) + ...
-        %        reshape(((1:sM(3)) - target(3)) * N_orth(3), [1, 1, sM(3)]);
-        % 
-        % segmented_img_to_plot(Dist>0) = 0;
         segmented_img_to_plot = segmented_img_to_plot(1:2:end,1:2:end,1:2:end);
 
         for_caps = segmented_img_to_plot;
@@ -127,7 +147,6 @@ h = figure('units', 'normalized', 'position', [0 0 1 1]);
 
         Ds = smooth3(gpuArray(double(segmented_img_to_plot>0)));
         skin_isosurface = isosurface(Ds,0.5);
-        %figure
         colormap(gray(80))
 
         hiso = patch(skin_isosurface,...
